e396173cbbf42bf7d7f94aca2746244c0b51012c
NatanElertSilva <joaovgp8147@gmail.com>
2024-05-29 16:38:38 -0300

    sim

--- TrabalhoMobile/App.js
+++ TrabalhoMobile/App.js
@@ -1,22 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Titulo from './componentes/Titulo';
+import CampoTexto from './componentes/CampoTexto';
+import Botao from './componentes/Botao';
+import { useState } from 'react';
 
 export default function App() {
-  return (
-    <View style = {styles.container}>
-      {/* <Text>teste</Text> */}
+  const [peso, setPeso] = useState(0.0);
+  const [altura, setAltura] = useState(0.0);
+  const [mensagem, setMensagem] = useState("")  
+
+  function mostrarMensagem(){
+    var imc = peso / (altura * altura )
+    setMensagem( "Peso: " + peso + "\nAltura: " + altura + "\nIMC: " + imc.toFixed(2) )
+  }
+
+  return (  
+    <View style = {styles.view}>
       <Titulo corTexto = "#f12" t={30} mensagem = "Calculadora de IMC"/>
+
+      <CampoTexto
+        funcao={setPeso}
+        titulo="Peso"
+        sugestao='Ex 75.3'
+        tipoTeclado='numeric'/>
+      <CampoTexto
+        funcao={setAltura}
+        titulo="Altura"
+        sugestao='Ex 1.85'
+        tipoTeclado='numeric'/>
+        <Botao cor="#ece" titulo="calcular"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'top',
-    marginTop: 30,
-  },
-});
+  view:{
+    width:'100%',
+    marginTop: '10%',
+    flex:1,
+    alignItems: 'center'
+  }
+})
--- TrabalhoMobile/componentes/CampoTexto.js
+++ TrabalhoMobile/componentes/CampoTexto.js
@@ -1,9 +1,33 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { React, useState } from "react";
+import { View, StyleSheet, Text, TextInput } from "react-native";
 
 export default function CampoTexto(props){
-    
-    return(
-
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>{props.titulo}</Text>
+            <TextInput 
+                style={styles.textInput}
+                placeholder={props.sugestao}
+                keyboardType={props.tipoTeclado}
+                onChangeText={(texto) => props.funcao(texto) }
+            />
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        width: '85%',
+        alignItems: 'center'
+    },
+    titulo:{
+        fontSize: 20
+    },
+    textInput: {
+      borderWidth: 1,
+      paddingLeft: 20,
+      paddingRight: 20,
+      width:'70%',
+      borderColor: 'black',
+    }
+  });
